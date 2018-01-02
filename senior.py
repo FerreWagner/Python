ee909b16014d853e8719e06b0a8fb955f709c724
FerreWagner <18408229270@163.com>
2018-01-02 17:43:05 +0800

    senior commit

--- senior.py
+++ senior.py
@@ -1,7 +1,17 @@
-L = []
-n = 1
-while n <= 99:
-    L.append(n)
-    n = n + 2
+L = ['faith', 'sccc', 'moggy', 'kpii', 'kaka']
 
-print(L)
+# c = L[0:4]  #L[0:4]表示，从索引0开始取，直到索引4为止，但不包括索引4
+# c = L[:4]
+# a = list(range(100))
+# c = L[-2:]
+# d = L[:]    #copy list L
+
+e = (0,9,8,7,6) #tuple cut
+
+# print('ASDDSA'[:3]);    #string cut
+print('ABCDRF'[::2]);       #2 step cut
+# print(e[:3])
+
+
+
+# iteration
