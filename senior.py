09bea2ff5d566421b6f720a503964c8bb5f8cd50
FerreWagner <18408229270@163.com>
2018-01-15 14:14:04 +0800

    ser commit

--- senior.py
+++ senior.py
@@ -6,12 +6,35 @@ L = ['faith', 'sccc', 'moggy', 'kpii', 'kaka']
 # c = L[-2:]
 # d = L[:]    #copy list L
 
-e = (0,9,8,7,6) #tuple cut
+# e = (0,9,8,7,6) #tuple cut
 
 # print('ASDDSA'[:3]);    #string cut
-print('ABCDRF'[::2]);       #2 step cut
+# print('ABCDRF'[::2])       #2 step cut
 # print(e[:3])
 
 
 
 # iteration
+# lis = [x * x for x in range(1, 11) if x % 2 == 0]
+# mn  = [m + n for m in 'KIU' for n in 'POU']
+# print(mn)
+
+#列出所有文件名和目录名
+# import os
+# dd = [d for d in os.listdir('.')]
+# print(dd)
+
+# for循环其实可以同时使用两个甚至多个变量
+# d = {'a':'A','b':'B','c':'C','d':'D'}
+# for k,v in d.items():
+#     print(k, '哈哈', v)
+
+# 列表生成式也可以使用两个变量来生成list
+# d = {'x': 'A', 'y': 'B', 'z': 'C' }
+# uu = [k + '=' + v for k, v in d.items()]
+# print(uu)
+
+L = ['Hello', 'World', 18, 'Apple', None]
+for s in L:
+    if isinstance(s, str):
+        print(s)
