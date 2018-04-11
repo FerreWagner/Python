d11a0724f826999e2fd1b26a1a8d0527c4a49dd7
FerreWagner <18408229270@163.com>
2018-04-11 11:47:44 +0800

    data success

--- data.py
+++ data.py
@@ -68,3 +68,46 @@ print(aa ^ bb) #在 aa 或 bb 中的字母，但不同时在 aa 和 bb 中
 # 集合也支持推导式
 sun = {x for x in 'abracadabra' if x not in 'abc'}
 print(sun)
+
+# 字典 在同一个字典之内，关键字必须是互不相同
+tel = {'sape': 40198, 'jake': 4139}
+print(list(tel.keys()))
+print(sorted(tel.keys()))
+
+# 构造函数 dict() 直接从键值对元组列表中构建字典。如果有固定的模式，列表推导式指定特定的键值对
+dict_name = dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
+print(dict_name)
+
+poem = {x: x**2 for x in (2, 4, 6)}
+print(poem)
+
+str_dict = dict(sape=4139, guido=4127, jack=4098)
+print(str_dict)
+
+forea = {'gallahad': 'the pure', 'robin': 'the brave'}
+for k, v in forea.items():    #在字典中遍历时，关键字和对应的值可以使用 items() 方法同时解读出来：
+     print(k, v)
+
+
+# -----------------------------------------------------------------------------------------------------------
+
+for i, v in enumerate(['tic', 'tac', 'toe']):     #在序列中遍历时，索引位置和对应值可以使用 enumerate() 函数同时得到：
+     print(i, v)
+
+
+
+# 同时遍历两个或更多的序列，可以使用 zip() 组合：
+questions = ['name', 'quest', 'favorite color']
+answers = ['lancelot', 'the holy grail', 'blue']
+for q, a in zip(questions, answers):
+     print('What is your {0}?  It is {1}.'.format(q, a))
+
+
+# 要反向遍历一个序列，首先指定这个序列，然后调用 reversed() 函数：
+for i in reversed(range(1, 10, 2)):
+     print(i)
+
+# 要按顺序遍历一个序列，使用 sorted() 函数返回一个已排序的序列，并不修改原值：
+babyb = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+for f in sorted(set(babyb)):
+     print(f)
