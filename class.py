b48afc79f03cb539c0520cfb1131d38ccb59d7ec
FerreWagner <18408229270@163.com>
2018-04-13 17:55:05 +0800

    class prepare

---
+++ class.py
@@ -0,0 +1,11 @@
+# self代表类的实例，而非类
+# 类的方法与普通的函数只有一个特别的区别——它们必须有一个额外的第一个参数名称, 按照惯例它的名称是 self
+
+class Test:
+    def prt(self):
+        print(self)
+        print(self.__class__)
+
+
+t = Test()
+t.prt()
