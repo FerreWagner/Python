1a5dedea811e39e5cf6b26fc25e0cc875b3955d8
FerreWagner <18408229270@163.com>
2018-04-16 11:36:29 +0800

    class success

--- class.py
+++ class.py
@@ -9,3 +9,40 @@ class Test:
 
 t = Test()
 t.prt()
+
+
+# 继承
+class first:
+    def __init__(self):
+        print('my name is first')
+
+
+class second(first):
+    def __init__(self):
+        print('my name is second')
+
+
+class_second = second()
+
+# 类的私有属性
+# __private_attrs：两个下划线开头，声明该属性为私有，不能在类地外部被使用或直接访问。在类内部的方法中使用时 self.__private_attrs。
+
+# 类的私有方法
+# __private_method：两个下划线开头，声明该方法为私有方法，只能在类的内部调用 ，不能在类地外部调用。self.__private_methods。
+
+# 类的专有方法：
+# __init__ : 构造函数，在生成对象时调用
+# __del__ : 析构函数，释放对象时使用
+# __repr__ : 打印，转换
+# __setitem__ : 按照索引赋值
+# __getitem__: 按照索引获取值
+# __len__: 获得长度
+# __cmp__: 比较运算
+# __call__: 函数调用
+# __add__: 加运算
+# __sub__: 减运算
+# __mul__: 乘运算
+# __div__: 除运算
+# __mod__: 求余运算
+# __pow__: 乘方
+
